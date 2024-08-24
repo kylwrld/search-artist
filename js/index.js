8a4fccaddee7d621b7e3bd1b0319f725207a419d
kylwrld <danielrochinhah@gmail.com>
2024-08-23 23:11:18 -0300

    first

---
+++ js/index.js
@@ -0,0 +1,30 @@
+const API_KEY = "ae0050c3ef5865c95d6982924c7de685"
+const API_URL = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist="
+const PARAMS = `&api_key=${API_KEY}&format=json&limit=10`
+
+const getTopTracks = async (artist) => {
+    const url = API_URL + artist + PARAMS
+    const response = await fetch(url)
+    const result = await response.json()
+    return result
+}
+
+document.getElementById("form").addEventListener("submit", async (e) => {
+    e.preventDefault()
+    const artist = e.target[0].value
+    const result = await getTopTracks(artist)
+    const tracks = result["toptracks"]["track"]
+    const list = document.getElementById("result")
+    tracks.forEach(element => {
+        let div = document.createElement("div");
+        div.classList.add("list-item")
+        const inner = `
+        <div class="list-center"><p>#${element["@attr"].rank}</p></div>
+        <div class="list-center"><p>${element.name}</p></div>
+        <div class="list-center"><p>${element.playcount}</p></div>
+        <div class="list-center"><p>${element.listeners}</p></div>
+        `
+        div.innerHTML = inner
+        list.appendChild(div)
+    });
+})
